275d85d9bb1c3656103bc653f71b5521ea0ab4ce
joaovml20 <joaolima15257@gmail.com>
2026-06-24 16:35:42 -0300

    adicionando as operacoes
    adicionando as operacoes como adicao, subtracao, multiplicacao e divisao.

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 "use strict"
+
+let numero1 = 10,numero2 = 20;
+
+let resposta = numero1 + numero2;
+//let resposta = numero1 - numero2;
+//let resposta = numero1 * numero2;
+//let resposta = numero1 / numero2;
+
 console.log("ola mundo")
+console.log(resposta);
